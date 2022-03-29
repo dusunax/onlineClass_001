29709d5d3b509c4d5fe8ff44c4b8830ec9013881
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-03-29 12:45:05 +0900

    ~289 ~chall7

--- Challanges/EJS_blog/app.js
+++ Challanges/EJS_blog/app.js
@@ -34,6 +34,11 @@ app.get('/contact', function(req, res){
     'contact', { contactContent: contactContent }
   );
 });
+app.get('/compose', function(req, res){
+  res.render(
+    'compose'
+  );
+});
 
 // post
 
