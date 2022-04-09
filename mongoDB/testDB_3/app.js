db38f816c32aa5ba0f00443f553e732ecabbeab5
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-04-09 12:52:40 +0900

    ~339

--- mongoDB/testDB_3/app.js
+++ mongoDB/testDB_3/app.js
@@ -1,3 +1,4 @@
+// # modules require
 const mongoose = require('mongoose');
 mongoose.connect(
   'mongodb://0.0.0.0:27017/test3'
@@ -6,7 +7,7 @@ mongoose.connection.on('error', (error) => {
   console.error('연결 에러: ', error);
 });
 
-// 자료구조 => 스키마 정의, 모델 선언
+// # 자료구조 => 스키마 정의, 모델 선언
 const fruitSchema = new mongoose.Schema ({
   _id: Number, // inc_field
   name: {
@@ -21,28 +22,58 @@ const fruitSchema = new mongoose.Schema ({
   },
   review: {
     type: String,
-    max: [50, '50자 이내로 입력하세요.']
+    minLength: 0,
+    maxLength: [50, '50자 이내로 입력하세요.']
   }
 });
 const Fruit = mongoose.model('Fruit', fruitSchema);
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: [1, 'age between 1~150'],
+    max: [150, 'age between 1~150']
+  },
+  favouritefruit: fruitSchema
+});
+const Person = mongoose.model('person', personSchema);
 
-// insert
+// # 값 설정 여기
 const fruit = new Fruit({
   _id: 10,
-  name: 'Bug Apple',
-  rating: 10,
-  review: "오류나는 맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛맛"
+  name: 'Happy Apple',
+  rating: 9,
+  review: "세상의 밝은 면을 바라보는 긍정적인 사과"
+});
+const person = new Person({
+  name: 'John',
+  age: '14',
+  favouritefruit: fruit
 });
 
-Fruit.insertMany([ fruit ], function(err, data){
-  if(err){
-    console.log("입력 에러"+err);
-  } else {
-    console.log('New data insert completed.');
-    // console.log(data);
-  }
-})
+// # 명령 1: 과일입력
+// Fruit.insertMany([ fruit ], function(err, data){
+//   if(err){
+//     console.log("입력 에러"+err);
+//   } else {
+//     console.log('New data insert completed.');
+//     // console.log(data);
+//   }
+// });
 
+// # 명령 1: 사람입력
+// person.save((err)=>{
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log('새 사람')
+//   }
+// });
+
+// # 삭제 여기서
 // Fruit.deleteOne({_id: 10}, function(err, data){
 //   if(err){
 //     console.log("명령 에러"+err);
@@ -51,7 +82,7 @@ Fruit.insertMany([ fruit ], function(err, data){
 //   }
 // });
 
-// find
+// # 전체 확인 find
 // Fruit.find((err, data)=>{
 //   if(err){
 //     console.log(err);
