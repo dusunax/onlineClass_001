cf4302158c0e3343ccfde01b76c7938d1a423409
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-05-18 21:03:24 +0900

    ~375 RESTful 섹션끝

--- Challanges/wikiAPI/app.js
+++ Challanges/wikiAPI/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
+const _ = require("lodash");
 
 // Setting
 app.set('view engine', 'ejs');
@@ -24,37 +25,79 @@ const articleSchema = {
 }
 const Article = mongoose.model("Article", articleSchema);
 
-// articles: get, post, delete
+// targeting all articles: get, post, delete
 app.route("/articles")
-  .get((req, res) => {
-    Article.find(function(err, foundArticles) {
-      if (err) {
-        res.send(err);
-      } else {
-        console.log("sended a JSON to response.");
-        res.send(foundArticles);
-      }
-    });
-  })
-.post((req, res) => {
-    const newArticle = new Article({
-      title: req.body.title,
-      content: req.body.content
-    });
-    newArticle.save((err) => {
-      !err ? res.send("article saved") : res.send(err);
-    });
-  })
-  .delete((req, res) => {
-    Article.deleteMany((err) => {
-      if (!err) {
-        res.send("successfully deleted all the articles.");
-      } else {
-        res.send(err);
-      }
-    });
+.get((req, res) => {
+  Article.find(function(err, foundArticles) {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log("sended a JSON to response.");
+      res.send(foundArticles);
+    }
   });
+})
+.post((req, res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save((err) => {
+    !err ? res.send("article saved") : res.send(err);
+  });
+})
+.delete((req, res) => {
+  Article.deleteMany((err) => {
+    if (!err) {
+      res.send("successfully deleted all the articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
 
+// targeting a specific article. (params)
+app.route("/articles/:title")
+.get((req, res)=>{
+  let currentParams=req.params.title;
+  Article.findOne({title: currentParams}, (err, foundArticle)=>{
+    if(foundArticle){
+      res.send(foundArticle);
+    } else {
+      res.send("해당 article을 찾을 수 없습니다.");
+    }
+  });
+})
+.put((req, res)=>{
+  // res.send("hello?")
+  Article.replaceOne(
+    {title: req.params.title},
+    {title: req.body.title, content: req.body.content},
+    (err)=>{
+      !err?res.send("Successfully update the document"):console.log(err);
+    }
+  );
+})
+.patch((req, res)=>{
+  // Article.updateOne(
+  Article.updateOne(
+    {title: req.params.title},
+    {$set: req.body},
+    (err)=>{
+      !err?res.send("Successfully patch the document"):console.log(err);
+    }
+  );
+})
+.delete((req, res)=>{
+  // Article.deleteOne({title: req.params.title}, (err)=>{
+  //   !err?res.send("Successfully delete the document"):console.log(err);
+  // });
+  // null지우기
+  Article.deleteMany({title: {$eq: null}}, {title: {$eq: ""}}, (err, foundArticle)=>{
+    res.send(foundArticle.deletedCount+"개 아이템 삭제")
+    !err?res.send("Successfully delete null documents"):console.log(err);
+  });
+});
 
 // Listen
 app.listen(3000, (err) => {
