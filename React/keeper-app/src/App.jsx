7e299226c006868e439cc3ffa20945d56f213b29
Dusuna <94776135+dusunax@users.noreply.github.com>
2022-06-19 17:10:45 +0900

    ~409 React: Lcoal App 시작하기

---
+++ React/keeper-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Note from './component/Note';
+
+function App(){
+    return <div>
+        <Header />
+        <Footer />
+        <Note />
+    </div>
+}
+
+export default App;
